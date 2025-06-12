f8d893d27a24d03f93fcac7f427c1b6aba75206a
archiesinaga <archiesinaga9@gmail.com>
2025-06-12 15:28:17 +0700

    kucing

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  webpack: (config) => {
+    config.resolve.fallback = { 
+      ...config.resolve.fallback,
+      tls: false,
+      net: false,
+      dns: false,
+      fs: false,
+    };
+    return config;
+  },
+};
